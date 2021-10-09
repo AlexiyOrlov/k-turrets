235d60b90a2759ac1ff083fea62b7d61abcd097b
alexiy <alexiy.ov@gmail.com>
2021-10-09 20:19:15 +0300

    Started Bullet turret

---
+++ src/main/java/dev/buildtool/kturrets/bullet/Bullet.java
@@ -0,0 +1,52 @@
+package dev.buildtool.kturrets.bullet;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.projectile.DamagingProjectileEntity;
+import net.minecraft.network.IPacket;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.math.EntityRayTraceResult;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.network.NetworkHooks;
+
+public class Bullet extends DamagingProjectileEntity {
+    private float damage;
+    static DamageSource damageSource = new DamageSource("k-turrets.bullet");
+
+    public Bullet(EntityType<? extends DamagingProjectileEntity> p_i50173_1_, World p_i50173_2_) {
+        super(p_i50173_1_, p_i50173_2_);
+    }
+
+    public Bullet(EntityType<? extends DamagingProjectileEntity> p_i50174_1_, double p_i50174_2_, double p_i50174_4_, double p_i50174_6_, double p_i50174_8_, double p_i50174_10_, double p_i50174_12_, World p_i50174_14_) {
+        super(p_i50174_1_, p_i50174_2_, p_i50174_4_, p_i50174_6_, p_i50174_8_, p_i50174_10_, p_i50174_12_, p_i50174_14_);
+    }
+
+    public Bullet(EntityType<? extends DamagingProjectileEntity> p_i50175_1_, LivingEntity p_i50175_2_, double p_i50175_3_, double p_i50175_5_, double p_i50175_7_, World p_i50175_9_, float damage) {
+        super(p_i50175_1_, p_i50175_2_, p_i50175_3_, p_i50175_5_, p_i50175_7_, p_i50175_9_);
+        this.damage = damage;
+    }
+
+    @Override
+    protected float getInertia() {
+        return 1;
+    }
+
+    @Override
+    public IPacket<?> getAddEntityPacket() {
+        return NetworkHooks.getEntitySpawningPacket(this);
+    }
+
+    @Override
+    protected void onHit(RayTraceResult p_70227_1_) {
+        super.onHit(p_70227_1_);
+        remove();
+    }
+
+    @Override
+    protected void onHitEntity(EntityRayTraceResult entityRayTraceResult) {
+        Entity entity = entityRayTraceResult.getEntity();
+        entity.hurt(damageSource, damage);
+    }
+}
---
+++ src/main/java/dev/buildtool/kturrets/bullet/BulletTurret.java
@@ -0,0 +1,84 @@
+package dev.buildtool.kturrets.bullet;
+
+import dev.buildtool.kturrets.Turret;
+import dev.buildtool.satako.ItemHandler;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.MobEntity;
+import net.minecraft.entity.ai.goal.NearestAttackableTargetGoal;
+import net.minecraft.entity.ai.goal.RangedAttackGoal;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.inventory.container.Container;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
+
+public class BulletTurret extends Turret {
+    protected final ItemHandler ammo = new ItemHandler(27) {
+        @Override
+        public ItemStack insertItem(int slot, ItemStack stack, boolean simulate) {
+            if (stack.getItem() == Items.IRON_NUGGET || stack.getItem() == Items.GOLD_NUGGET)
+                return super.insertItem(slot, stack, simulate);
+            return stack;
+        }
+    };
+
+    public BulletTurret(EntityType<? extends MobEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    @Override
+    protected void registerGoals() {
+        goalSelector.addGoal(5, new RangedAttackGoal(this, 0, 13, (float) getRange()));
+        targetSelector.addGoal(5, new NearestAttackableTargetGoal(this, LivingEntity.class, 0, true, true,
+                livingEntity -> {
+                    if (livingEntity instanceof LivingEntity) {
+                        LivingEntity livingEntity1 = (LivingEntity) livingEntity;
+                        return decodeTargets(getTargets()).contains(livingEntity1);
+                    }
+                    return false;
+                }) {
+            @Override
+            public boolean canUse() {
+                return !ammo.isEmpty() && super.canUse();
+            }
+        });
+    }
+
+    @Override
+    public void performRangedAttack(LivingEntity livingEntity, float v) {
+        if (livingEntity.isAlive()) {
+            for (ItemStack item : ammo.getItems()) {
+                double d0 = livingEntity.getX() - this.getX();
+                double d1 = livingEntity.getEyeY() - getEyeY();
+                double d2 = livingEntity.getZ() - this.getZ();
+                Bullet bullet = new Bullet(null, this, d0, d1, d2, level, item.getItem() == Items.GOLD_NUGGET ? 6 : 7);
+                level.addFreshEntity(bullet);
+                item.shrink(1);
+                break;
+            }
+        }
+    }
+
+    @Nullable
+    @Override
+    public Container createMenu(int i, PlayerInventory playerInventory, PlayerEntity playerEntity) {
+        return null;
+    }
+
+    @Override
+    public void addAdditionalSaveData(CompoundNBT compoundNBT) {
+        super.addAdditionalSaveData(compoundNBT);
+        compoundNBT.put("Ammo", ammo.serializeNBT());
+    }
+
+    @Override
+    public void readAdditionalSaveData(CompoundNBT compoundNBT) {
+        super.readAdditionalSaveData(compoundNBT);
+        ammo.deserializeNBT(compoundNBT.getCompound("Ammo"));
+    }
+}
