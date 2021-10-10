30fac18b73ce2f6f529d2ba879aebf79823f849a
alexiy <alexiy.ov@gmail.com>
2021-10-10 12:51:27 +0300

    Fixed the container

--- src/main/java/dev/buildtool/kturrets/bullet/BulletScreen.java
+++ src/main/java/dev/buildtool/kturrets/bullet/BulletScreen.java
@@ -2,11 +2,10 @@ package dev.buildtool.kturrets.bullet;
 
 import dev.buildtool.satako.gui.ContainerScreen2;
 import net.minecraft.entity.player.PlayerInventory;
-import net.minecraft.inventory.container.Container;
 import net.minecraft.util.text.ITextComponent;
 
-public class BulletScreen extends ContainerScreen2 {
-    public BulletScreen(Container container, PlayerInventory playerInventory, ITextComponent name, boolean drawBorders_) {
+public class BulletScreen extends ContainerScreen2<BulletTurretContainer> {
+    public BulletScreen(BulletTurretContainer container, PlayerInventory playerInventory, ITextComponent name, boolean drawBorders_) {
         super(container, playerInventory, name, drawBorders_);
     }
 }
--- src/main/java/dev/buildtool/kturrets/registers/ClientSetup.java
+++ src/main/java/dev/buildtool/kturrets/registers/ClientSetup.java
@@ -5,6 +5,8 @@ import dev.buildtool.kturrets.arrow.ArrowTurretModel;
 import dev.buildtool.kturrets.arrow.ArrowTurretRenderer;
 import dev.buildtool.kturrets.arrow.ArrowTurretScreen;
 import dev.buildtool.kturrets.bullet.BulletRenderer;
+import dev.buildtool.kturrets.bullet.BulletScreen;
+import dev.buildtool.kturrets.bullet.BulletTurretContainer;
 import net.minecraft.client.gui.ScreenManager;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -19,7 +21,7 @@ public class ClientSetup {
     @SubscribeEvent
     public static void register(FMLClientSetupEvent clientSetupEvent) {
         ScreenManager.register(TContainers.ARROW_TURRET, (ScreenManager.IScreenFactory<ArrowTurretContainer, ArrowTurretScreen>) (p_create_1_, p_create_2_, p_create_3_) -> new ArrowTurretScreen(p_create_1_, p_create_2_, p_create_3_, true));
-//        ScreenManager.register(TContainers.BULLET_TURRET,(ScreenManager.IScreenFactory<BulletTurretContainer, BulletScreen>) (p1, p2, p3) -> new BulletScreen(p1, p2, p3, true));
+        ScreenManager.register(TContainers.BULLET_TURRET, (ScreenManager.IScreenFactory<BulletTurretContainer, BulletScreen>) (p1, p2, p3) -> new BulletScreen(p1, p2, p3, true));
         RenderingRegistry.registerEntityRenderingHandler(TEntities.ARROW_TURRET, manager -> new ArrowTurretRenderer(manager, new ArrowTurretModel(), "arrowturret", false, 0.6f));
         RenderingRegistry.registerEntityRenderingHandler(TEntities.BULLET, BulletRenderer::new);
     }
