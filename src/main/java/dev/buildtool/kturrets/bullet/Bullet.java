8d2db93ccc83f4f8a3f510422471540b49562f4a
alexiy <alexiy.ov@gmail.com>
2021-10-11 10:54:57 +0300

    Don't burn; remove on hit

--- src/main/java/dev/buildtool/kturrets/bullet/Bullet.java
+++ src/main/java/dev/buildtool/kturrets/bullet/Bullet.java
@@ -6,18 +6,17 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.projectile.DamagingProjectileEntity;
 import net.minecraft.network.IPacket;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.math.EntityRayTraceResult;
-import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.network.NetworkHooks;
 
 public class Bullet extends DamagingProjectileEntity {
-    private float damage;
+    private float damage; //sync?
     static DamageSource damageSource = new DamageSource("k-turrets.bullet");
 
     public Bullet(World p_i50173_2_) {
         super(TEntities.BULLET, p_i50173_2_);
-        setCustomNameVisible(false);
     }
 
     public Bullet(double p_i50174_2_, double p_i50174_4_, double p_i50174_6_, double p_i50174_8_, double p_i50174_10_, double p_i50174_12_, World p_i50174_14_) {
@@ -27,6 +26,11 @@ public class Bullet extends DamagingProjectileEntity {
     public Bullet(LivingEntity p_i50175_2_, double p_i50175_3_, double p_i50175_5_, double p_i50175_7_, World p_i50175_9_, float damage) {
         super(TEntities.BULLET, p_i50175_2_, p_i50175_3_, p_i50175_5_, p_i50175_7_, p_i50175_9_);
         this.damage = damage;
+    }
+
+    @Override
+    protected boolean shouldBurn() {
+        return false;
     }
 
     @Override
@@ -40,15 +44,16 @@ public class Bullet extends DamagingProjectileEntity {
     }
 
     @Override
-    protected void onHit(RayTraceResult p_70227_1_) {
-        super.onHit(p_70227_1_);
+    protected void onHitEntity(EntityRayTraceResult entityRayTraceResult) {
+        Entity entity = entityRayTraceResult.getEntity();
+        entity.hurt(damageSource, damage);
         remove();
     }
 
     @Override
-    protected void onHitEntity(EntityRayTraceResult entityRayTraceResult) {
-        Entity entity = entityRayTraceResult.getEntity();
-        entity.hurt(damageSource, damage);
+    protected void onHitBlock(BlockRayTraceResult p_230299_1_) {
+        super.onHitBlock(p_230299_1_);
+        remove();
     }
 
     public float getDamage() {
