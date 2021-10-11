57a54d264a6c153c438b630ec3ce3598fe88ea95
alexiy <alexiy.ov@gmail.com>
2021-10-11 19:34:38 +0300

    Added steel ingot and semi-steel product

--- src/main/java/dev/buildtool/kturrets/registers/TItems.java
+++ src/main/java/dev/buildtool/kturrets/registers/TItems.java
@@ -1,7 +1,11 @@
 package dev.buildtool.kturrets.registers;
 
 import dev.buildtool.kturrets.KTurrets;
-import net.minecraft.item.*;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.SpawnEggItem;
+import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 
 public class TItems {
@@ -9,13 +13,21 @@ public class TItems {
     public static final ItemGroup ITEM_GROUP = new ItemGroup("k-turrets") {
         @Override
         public ItemStack makeIcon() {
-            return new ItemStack(Items.AIR);
+            return new ItemStack(SEMI_STEEL.get());
         }
     };
+    public static RegistryObject<Item> SEMI_STEEL;
+    public static RegistryObject<Item> STEEL_INGOT;
 
     static {
-        ITEMS.register("arrow_turret_item", () -> new SpawnEggItem(TEntities.ARROW_TURRET, 0x0, 0x0, new Item.Properties().tab(ITEM_GROUP)));
-        ITEMS.register("bullet_turret_item", () -> new SpawnEggItem(TEntities.BULLET_TURRET, 0x0, 0x0, new Item.Properties().tab(ITEM_GROUP)));
-        ITEMS.register("firecharge_turret_item", () -> new SpawnEggItem(TEntities.FIRE_CHARGE_TURRET, 0x0, 0x0, new Item.Properties().tab(ITEM_GROUP)));
+        ITEMS.register("arrow_turret_item", () -> new SpawnEggItem(TEntities.ARROW_TURRET, 0x0, 0x0, defaults()));
+        ITEMS.register("bullet_turret_item", () -> new SpawnEggItem(TEntities.BULLET_TURRET, 0x0, 0x0, defaults()));
+        ITEMS.register("firecharge_turret_item", () -> new SpawnEggItem(TEntities.FIRE_CHARGE_TURRET, 0x0, 0x0, defaults()));
+        SEMI_STEEL = ITEMS.register("semi_steel", () -> new Item(defaults()));
+        STEEL_INGOT = ITEMS.register("steel_ingot", () -> new Item(defaults()));
+    }
+
+    private static Item.Properties defaults() {
+        return new Item.Properties().tab(ITEM_GROUP);
     }
 }
