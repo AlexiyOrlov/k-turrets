3b07a0a8f388a3e47bdb3fe705a584d99bfc2eca
alexiy <alexiy.ov@gmail.com>
2021-10-14 16:43:20 +0300

    Add armor

--- src/main/java/dev/buildtool/kturrets/Turret.java
+++ src/main/java/dev/buildtool/kturrets/Turret.java
@@ -56,7 +56,7 @@ public abstract class Turret extends MobEntity implements IRangedAttackMob, INam
     }
 
     public static AttributeModifierMap.MutableAttribute createDefaultAttributes() {
-        return createLivingAttributes().add(Attributes.FOLLOW_RANGE, 32).add(Attributes.MOVEMENT_SPEED, 0).add(Attributes.MAX_HEALTH, 60).add(Attributes.ATTACK_DAMAGE, 4);
+        return createLivingAttributes().add(Attributes.FOLLOW_RANGE, 32).add(Attributes.MOVEMENT_SPEED, 0).add(Attributes.MAX_HEALTH, 60).add(Attributes.ATTACK_DAMAGE, 4).add(Attributes.ARMOR, 3);
     }
 
     @Override
