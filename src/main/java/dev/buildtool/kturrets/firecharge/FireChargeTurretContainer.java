f38304da8c6c48237b5ac8926a94f32039ab7f11
alexiy <alexiy.ov@gmail.com>
2021-10-24 08:13:18 +0300

    Quick stack move

--- src/main/java/dev/buildtool/kturrets/firecharge/FireChargeTurretContainer.java
+++ src/main/java/dev/buildtool/kturrets/firecharge/FireChargeTurretContainer.java
@@ -3,7 +3,10 @@ package dev.buildtool.kturrets.firecharge;
 import dev.buildtool.kturrets.registers.TContainers;
 import dev.buildtool.satako.Container2;
 import dev.buildtool.satako.ItemHandlerSlot;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.network.PacketBuffer;
 
 public class FireChargeTurretContainer extends Container2 {
@@ -18,5 +21,18 @@ public class FireChargeTurretContainer extends Container2 {
         }
 
         addPlayerInventory(0, 4 * 18, playerInventory);
+    }
+
+    @Override
+    public ItemStack quickMoveStack(PlayerEntity playerIn, int index) {
+        ItemStack itemStack = getSlot(index).getItem();
+        if (index > 26) {
+            if (itemStack.getItem() == Items.FIRE_CHARGE && !moveItemStackTo(itemStack, 0, 27, false))
+                return ItemStack.EMPTY;
+        } else {
+            if (!moveItemStackTo(itemStack, 27, 63, false))
+                return ItemStack.EMPTY;
+        }
+        return super.quickMoveStack(playerIn, index);
     }
 }
