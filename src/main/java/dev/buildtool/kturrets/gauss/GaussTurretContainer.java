2e636bd76e1d750580e376ebc6c3ff0511417e3f
alexiy <alexiy.ov@gmail.com>
2021-10-24 08:09:17 +0300

    Quick stack move

--- src/main/java/dev/buildtool/kturrets/gauss/GaussTurretContainer.java
+++ src/main/java/dev/buildtool/kturrets/gauss/GaussTurretContainer.java
@@ -1,9 +1,12 @@
 package dev.buildtool.kturrets.gauss;
 
 import dev.buildtool.kturrets.registers.TContainers;
+import dev.buildtool.kturrets.registers.TItems;
 import dev.buildtool.satako.Container2;
 import dev.buildtool.satako.ItemHandlerSlot;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.ItemStack;
 import net.minecraft.network.PacketBuffer;
 
 public class GaussTurretContainer extends Container2 {
@@ -18,5 +21,20 @@ public class GaussTurretContainer extends Container2 {
         }
 
         addPlayerInventory(0, 4 * 18, inventory);
+    }
+
+    @Override
+    public ItemStack quickMoveStack(PlayerEntity playerIn, int index) {
+        ItemStack stack = getSlot(index).getItem();
+        ItemStack stack1 = ItemStack.EMPTY;
+        if (stack.getItem() == TItems.GAUSS_BULLET.get() && index > 26) {
+            stack1 = stack.copy();
+            if (!moveItemStackTo(stack, 0, 27, false))
+                return ItemStack.EMPTY;
+        } else if (index < 27) {
+            if (!moveItemStackTo(stack, 27, 63, false))
+                return ItemStack.EMPTY;
+        }
+        return stack1;
     }
 }
