d28e27f6bb94aace4a6c4107878dd9448b6cdec1
alexiy <alexiy.ov@gmail.com>
2021-10-24 08:15:13 +0300

    Quick stack move

--- src/main/java/dev/buildtool/kturrets/bullet/BulletTurretContainer.java
+++ src/main/java/dev/buildtool/kturrets/bullet/BulletTurretContainer.java
@@ -3,7 +3,10 @@ package dev.buildtool.kturrets.bullet;
 import dev.buildtool.kturrets.registers.TContainers;
 import dev.buildtool.satako.Container2;
 import dev.buildtool.satako.ItemHandlerSlot;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.network.PacketBuffer;
 
 public class BulletTurretContainer extends Container2 {
@@ -17,5 +20,18 @@ public class BulletTurretContainer extends Container2 {
             }
         }
         addPlayerInventory(0, 5 * 18, playerInventory);
+    }
+
+    @Override
+    public ItemStack quickMoveStack(PlayerEntity playerIn, int index) {
+        ItemStack itemStack = getSlot(index).getItem();
+        if (index > 26) {
+            if ((itemStack.getItem() == Items.IRON_NUGGET || itemStack.getItem() == Items.GOLD_NUGGET) && !moveItemStackTo(itemStack, 0, 27, false))
+                return ItemStack.EMPTY;
+        } else {
+            if (!moveItemStackTo(itemStack, 27, 63, false))
+                return ItemStack.EMPTY;
+        }
+        return super.quickMoveStack(playerIn, index);
     }
 }
