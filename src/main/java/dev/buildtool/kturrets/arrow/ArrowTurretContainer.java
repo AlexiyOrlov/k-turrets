9732e54cb720e6611b998be30d0b5412b3479813
alexiy <alexiy.ov@gmail.com>
2021-10-24 08:20:53 +0300

    Quick stack move

--- src/main/java/dev/buildtool/kturrets/arrow/ArrowTurretContainer.java
+++ src/main/java/dev/buildtool/kturrets/arrow/ArrowTurretContainer.java
@@ -3,7 +3,12 @@ package dev.buildtool.kturrets.arrow;
 import dev.buildtool.kturrets.registers.TContainers;
 import dev.buildtool.satako.Container2;
 import dev.buildtool.satako.ItemHandlerSlot;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.ArrowItem;
+import net.minecraft.item.BowItem;
+import net.minecraft.item.CrossbowItem;
+import net.minecraft.item.ItemStack;
 import net.minecraft.network.PacketBuffer;
 
 public class ArrowTurretContainer extends Container2 {
@@ -19,5 +24,20 @@ public class ArrowTurretContainer extends Container2 {
         }
 
         addPlayerInventory(0, 5 * 18, playerInventory);
+    }
+
+    @Override
+    public ItemStack quickMoveStack(PlayerEntity playerIn, int index) {
+        ItemStack itemStack = getSlot(index).getItem();
+        if (index > 27) {
+            if ((itemStack.getItem() instanceof BowItem || itemStack.getItem() instanceof CrossbowItem) && !moveItemStackTo(itemStack, 0, 1, false))
+                return ItemStack.EMPTY;
+            if (itemStack.getItem() instanceof ArrowItem && !moveItemStackTo(itemStack, 1, 28, false))
+                return ItemStack.EMPTY;
+        } else {
+            if (!moveItemStackTo(itemStack, 28, 64, false))
+                return ItemStack.EMPTY;
+        }
+        return super.quickMoveStack(playerIn, index);
     }
 }
