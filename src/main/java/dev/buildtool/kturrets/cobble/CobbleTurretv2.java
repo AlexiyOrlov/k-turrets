69c83f7d4de653863f5b1ecfcecd9ac0adcdf83d
alexiy <alexiy.ov@gmail.com>
2022-04-30 11:39:32 +0300

    Deprecate previous models

--- src/main/java/dev/buildtool/kturrets/cobble/CobbleTurretv2.java
+++ src/main/java/dev/buildtool/kturrets/cobble/CobbleTurretv2.java
@@ -15,7 +15,7 @@ import net.minecraft.client.model.geom.PartPose;
 import net.minecraft.client.model.geom.builders.*;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
-
+@Deprecated
 public class CobbleTurretv2<T extends Entity> extends EntityModel<T> {
     // This layer location should be baked with EntityRendererProvider.Context in the entity renderer and passed into this model's constructor
     public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(new ResourceLocation(KTurrets.ID, "cobble_turret"), "main");
