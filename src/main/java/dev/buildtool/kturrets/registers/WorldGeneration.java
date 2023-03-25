b9e2255de7211538d5c815d1159dc0667ce44c14
alexiy <alexiy.ov@gmail.com>
2023-03-25 21:50:29 +0200

    Increase generation

--- src/main/java/dev/buildtool/kturrets/registers/WorldGeneration.java
+++ src/main/java/dev/buildtool/kturrets/registers/WorldGeneration.java
@@ -22,11 +22,11 @@ public class WorldGeneration {
     public static final DeferredRegister<ConfiguredFeature<?, ?>> CONFIGURED_FEATURE_REGISTER = DeferredRegister.create(Registry.CONFIGURED_FEATURE_REGISTRY, KTurrets.ID);
     static final Supplier<List<OreConfiguration.TargetBlockState>> TARGET_BLOCKSTATES = Suppliers.memoize(() -> List.of(OreConfiguration.target(OreFeatures.STONE_ORE_REPLACEABLES, KBlocks.TITANIUM_ORE.get().defaultBlockState()), OreConfiguration.target(OreFeatures.DEEPSLATE_ORE_REPLACEABLES, KBlocks.DEEP_SLATE_TITANIUM_ORE.get().defaultBlockState())));
 
-    public static final RegistryObject<ConfiguredFeature<?, ?>> CONFIGURED_ORE_FEATURE = CONFIGURED_FEATURE_REGISTER.register("titanium_ore", () -> new ConfiguredFeature<>(Feature.ORE, new OreConfiguration(TARGET_BLOCKSTATES.get(), 5)));
+    public static final RegistryObject<ConfiguredFeature<?, ?>> CONFIGURED_ORE_FEATURE = CONFIGURED_FEATURE_REGISTER.register("titanium_ore", () -> new ConfiguredFeature<>(Feature.ORE, new OreConfiguration(TARGET_BLOCKSTATES.get(), 11)));
 
     public static final DeferredRegister<PlacedFeature> PLACED_FEATURE_REGISTER = DeferredRegister.create(Registry.PLACED_FEATURE_REGISTRY, KTurrets.ID);
 
     static {
-        PLACED_FEATURE_REGISTER.register("titanium_ore", () -> new PlacedFeature(CONFIGURED_ORE_FEATURE.getHolder().get(), List.of(CountPlacement.of(4), InSquarePlacement.spread(), HeightRangePlacement.triangle(VerticalAnchor.bottom(), VerticalAnchor.absolute(384)))));
+        PLACED_FEATURE_REGISTER.register("titanium_ore", () -> new PlacedFeature(CONFIGURED_ORE_FEATURE.getHolder().get(), List.of(CountPlacement.of(35), InSquarePlacement.spread(), HeightRangePlacement.triangle(VerticalAnchor.bottom(), VerticalAnchor.absolute(384)))));
     }
 }
