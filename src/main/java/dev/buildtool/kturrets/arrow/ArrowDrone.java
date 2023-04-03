553fc462759feb02d9e7e81fb32e5e6e5ec9dc24
alexiy <alexiy.ov@gmail.com>
2023-04-03 07:15:00 +0300

    Increase arrow speed

--- src/main/java/dev/buildtool/kturrets/arrow/ArrowDrone.java
+++ src/main/java/dev/buildtool/kturrets/arrow/ArrowDrone.java
@@ -92,6 +92,7 @@ public class ArrowDrone extends Drone {
                         }
                         arrow2.setEnchantmentEffectsFromEntity(this, distanceFactor);
                         arrow2.setNoGravity(true);
+                        arrow2.shoot(d0, d1, d2, 1.8f, 0);
                         this.playSound(SoundEvents.SKELETON_SHOOT, 1.0F, 1.0F / (this.random.nextFloat() * 0.4F + 0.8F));
                         this.level.addFreshEntity(arrow2);
                         arrows.shrink(1);
