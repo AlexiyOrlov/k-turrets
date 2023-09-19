f2a9c58f12096f043b2354b0155c1cd1c504a9a7
alexiy <alexiy.ov@gmail.com>
2023-09-19 22:34:07 +0300

    Implement drone location highlight

--- src/main/java/dev/buildtool/kturrets/ClientEvents.java
+++ src/main/java/dev/buildtool/kturrets/ClientEvents.java
@@ -2,17 +2,31 @@ package dev.buildtool.kturrets;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.math.Vector3f;
+import dev.buildtool.satako.Constants;
+import dev.buildtool.satako.IntegerColor;
+import dev.buildtool.satako.Methods;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RenderGuiOverlayEvent;
+import net.minecraftforge.client.event.RenderLevelStageEvent;
 import net.minecraftforge.client.event.RenderLivingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.loading.LoadingModList;
 
+import java.util.List;
+
 @Mod.EventBusSubscriber(Dist.CLIENT)
 public class ClientEvents {
+    static boolean noDronesNearby;
     @SubscribeEvent
     public static void renderHealthIndicator(RenderLivingEvent.Post<?, ?> renderLivingEvent) {
         if (LoadingModList.get().getModFileById("neat") == null && KTurrets.SHOW_INTEGRITY.get()) {
@@ -29,6 +43,42 @@ public class ClientEvents {
                 renderLivingEvent.getRenderer().getFont().draw(renderLivingEvent.getPoseStack(), health, 0, 0, livingEntity.getHealth() < livingEntity.getMaxHealth() / 2 ? ChatFormatting.RED.getColor() : ChatFormatting.GREEN.getColor());
                 poseStack.popPose();
             }
+        }
+    }
+
+    @SubscribeEvent
+    public static void renderDroneLocations(RenderLevelStageEvent renderLevelStageEvent) {
+        if (ClientModEvents.highlightDronePositions.isDown() && renderLevelStageEvent.getStage() == RenderLevelStageEvent.Stage.AFTER_PARTICLES) {
+            Minecraft minecraft = Minecraft.getInstance();
+            Player player = minecraft.player;
+            List<Drone> nearbyDrones = minecraft.level.getEntitiesOfClass(Drone.class, new AABB(player.blockPosition()).inflate(64), drone -> drone.getOwner().isPresent() && drone.getOwner().get().equals(player.getUUID()));
+            if (!nearbyDrones.isEmpty()) {
+                noDronesNearby = false;
+                MultiBufferSource.BufferSource bufferSource = minecraft.renderBuffers().bufferSource();
+                PoseStack poseStack = renderLevelStageEvent.getPoseStack();
+                Vec3 projectedView = minecraft.gameRenderer.getMainCamera().getPosition();
+                poseStack.pushPose();
+                poseStack.translate(-projectedView.x, -projectedView.y, -projectedView.z);
+                IntegerColor orange = new IntegerColor(0x40FF5790);
+                nearbyDrones.forEach(drone -> {
+                    Vec3 dronePosition = drone.getPosition(1);
+                    poseStack.translate(dronePosition.x - 0.5, dronePosition.y - 0.2, dronePosition.z - 0.5);
+                    Methods.addRectangle(bufferSource.getBuffer(RenderType.lightning()), poseStack.last().pose(), 0, 0, 0, orange.getRed(), orange.getGreen(), orange.getBlue(), orange.getAlpha(), false, 0);
+                    poseStack.translate(-(dronePosition.x - 0.5), -(dronePosition.y - 0.2), -(dronePosition.z - 0.5));
+                });
+                poseStack.popPose();
+            } else
+                noDronesNearby = true;
+        }
+    }
+
+    @SubscribeEvent
+    public static void showWarning(RenderGuiOverlayEvent.Post renderGuiOverlayEvent) {
+        if (ClientModEvents.highlightDronePositions.isDown() && noDronesNearby) {
+            Minecraft minecraft = Minecraft.getInstance();
+            int screenWidth = renderGuiOverlayEvent.getWindow().getGuiScaledWidth();
+            Component warning = Component.translatable(KTurrets.ID + ".no.drones.nearby");
+            Methods.drawString(renderGuiOverlayEvent.getPoseStack(), warning.getString(), screenWidth / 2 - minecraft.font.width(warning) / 2, 20, Constants.GREEN);
         }
     }
 }
