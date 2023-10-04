73feb6058ad0eb24cb8deb75f93a5537299c26e4
alexiy <alexiy.ov@gmail.com>
2023-10-04 22:46:38 +0300

    Show indicator if turret has no owner or only to owner and allied players

--- src/main/java/dev/buildtool/kturrets/ClientEvents.java
+++ src/main/java/dev/buildtool/kturrets/ClientEvents.java
@@ -30,24 +30,28 @@ public class ClientEvents {
     public static void renderHealth(RenderLivingEvent.Post<?, ?> renderLivingEvent) {
         if (KTurrets.SHOW_INTEGRITY.get()) {
             LivingEntity livingEntity = renderLivingEvent.getEntity();
+            PlayerEntity player = Minecraft.getInstance().player;
             if (livingEntity instanceof Turret) {
-                MatrixStack matrixStack = renderLivingEvent.getMatrixStack();
-                matrixStack.pushPose();
-                String health = String.format("%.1f", livingEntity.getHealth()) + "/" + (int) livingEntity.getMaxHealth();
-                matrixStack.scale(0.03f, 0.03f, 0.03f);
-                matrixStack.mulPose(Minecraft.getInstance().getEntityRenderDispatcher().cameraOrientation());
-                matrixStack.mulPose(Vector3f.YP.rotationDegrees(180));
-                matrixStack.mulPose(Vector3f.XP.rotationDegrees(180));
-                matrixStack.translate(-renderLivingEvent.getRenderer().getFont().width(health) / 2f, -30 - livingEntity.getBbHeight() * 30, 0);
-                renderLivingEvent.getRenderer().getFont().draw(matrixStack, health, 0, 0, livingEntity.getHealth() < livingEntity.getMaxHealth() / 2 ? TextFormatting.RED.getColor() : TextFormatting.GREEN.getColor());
-                matrixStack.popPose();
+                Turret turret = (Turret) livingEntity;
+                if ((turret.getOwner().isPresent() && (turret.getOwner().equals(player.getUUID()) || player.isAlliedTo(turret))) || !turret.getOwner().isPresent()) {
+                    MatrixStack matrixStack = renderLivingEvent.getMatrixStack();
+                    matrixStack.pushPose();
+                    String health = String.format("%.1f", livingEntity.getHealth()) + "/" + (int) livingEntity.getMaxHealth();
+                    matrixStack.scale(0.03f, 0.03f, 0.03f);
+                    matrixStack.mulPose(Minecraft.getInstance().getEntityRenderDispatcher().cameraOrientation());
+                    matrixStack.mulPose(Vector3f.YP.rotationDegrees(180));
+                    matrixStack.mulPose(Vector3f.XP.rotationDegrees(180));
+                    matrixStack.translate(-renderLivingEvent.getRenderer().getFont().width(health) / 2f, -30 - livingEntity.getBbHeight() * 30, 0);
+                    renderLivingEvent.getRenderer().getFont().draw(matrixStack, health, 0, 0, livingEntity.getHealth() < livingEntity.getMaxHealth() / 2 ? TextFormatting.RED.getColor() : TextFormatting.GREEN.getColor());
+                    matrixStack.popPose();
+                }
             }
         }
     }
 
     @SubscribeEvent
     public static void renderDroneLocations(RenderWorldLastEvent renderLevelStageEvent) {
-        if (ClientModEvents.highlightDronePositions.isDown() /*&& renderLevelStageEvent.get == RenderLevelStageEvent.Stage.AFTER_CUTOUT_BLOCKS*/) {
+        if (ClientModEvents.highlightDronePositions.isDown()) {
             Minecraft minecraft = Minecraft.getInstance();
             PlayerEntity player = minecraft.player;
             List<Drone> nearbyDrones = minecraft.level.getEntitiesOfClass(Drone.class, new AxisAlignedBB(player.blockPosition()).inflate(64), drone -> drone.getOwner().isPresent() && drone.getOwner().get().equals(player.getUUID()));
