9f8b6311eb656c8cfe24369c761978efc70460d7
alexiy <alexiy.ov@gmail.com>
2023-10-18 18:45:23 +0300

    Fix not hitting 'friendly' target

--- src/main/java/dev/buildtool/kturrets/PresetProjectile.java
+++ src/main/java/dev/buildtool/kturrets/PresetProjectile.java
@@ -67,12 +67,15 @@ public abstract class PresetProjectile extends AbstractHurtingProjectile {
     @Override
     protected boolean canHitEntity(Entity target) {
         Entity owner = getOwner();
-        if (turret != null && target.getType().getCategory().isFriendly() && Turret.decodeTargets(turret.getTargets()).contains(target.getType()))
-            return super.canHitEntity(target);
+        if (turret != null) {
+            if (target.getType().getCategory().isFriendly() && (Turret.decodeTargets(turret.getTargets()).contains(target.getType()) || target == turret.getTarget()))
+                return super.canHitEntity(target);
+        }
         else if (owner == null || !owner.isAlliedTo(target) && !target.getType().getCategory().isFriendly()) {
             return super.canHitEntity(target);
         } else
             return turret == null || Turret.decodeTargets(turret.getTargets()).contains(target.getType()) || !target.getType().getCategory().isFriendly();
+        return true;
     }
 
     @Override
