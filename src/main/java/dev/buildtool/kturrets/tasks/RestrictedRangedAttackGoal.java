927c8f03f0443274c834fe041a4c99fd53bd98c9
AlexiyOrlov <alexiy.ov@gmail.com>
2024-11-06 17:39:58 +0200

    Set target

--- src/main/java/dev/buildtool/kturrets/tasks/RestrictedRangedAttackGoal.java
+++ src/main/java/dev/buildtool/kturrets/tasks/RestrictedRangedAttackGoal.java
@@ -23,6 +23,7 @@ public class RestrictedRangedAttackGoal extends RangedAttackGoal {
             float distanceToPlayer = drone.distanceTo(owner);
             if (distanceToPlayer < KTurrets.OWNER_FOLLOW_DISTANCE.get())
                 super.tick();
+            else drone.setTarget(null);
         }
     }
 }
