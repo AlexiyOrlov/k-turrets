42ba8686ff658f5a859eb899b99133f3479d3a44
AlexiyOrlov <alexiy.ov@gmail.com>
2024-11-16 22:56:16 +0200

    Move above target

--- src/main/java/dev/buildtool/kturrets/tasks/RestrictedRangedAttackGoal.java
+++ src/main/java/dev/buildtool/kturrets/tasks/RestrictedRangedAttackGoal.java
@@ -2,6 +2,7 @@ package dev.buildtool.kturrets.tasks;
 
 import dev.buildtool.kturrets.Drone;
 import dev.buildtool.kturrets.KTurrets;
+import net.minecraft.util.Mth;
 import net.minecraft.world.entity.ai.goal.RangedAttackGoal;
 import net.minecraft.world.entity.monster.RangedAttackMob;
 import net.minecraft.world.entity.player.Player;
@@ -18,13 +19,43 @@ public class RestrictedRangedAttackGoal extends RangedAttackGoal {
     public void tick() {
         Player owner = drone.level().getPlayerByUUID(drone.getOwnerUUID());
         if (owner == null)
-            super.tick();
+            work();
         else {
             float distanceToPlayer = drone.distanceTo(owner);
             if (distanceToPlayer < KTurrets.OWNER_FOLLOW_DISTANCE.get())
-                super.tick();
+                work();
             else if (drone.getTarget() != null)
                 drone.setTarget(null);
+        }
+    }
+
+    private void work() {
+        double d0 = this.mob.distanceToSqr(this.target.getX(), this.target.getY(), this.target.getZ());
+        boolean flag = this.mob.getSensing().hasLineOfSight(this.target);
+        if (flag) {
+            ++this.seeTime;
+        } else {
+            this.seeTime = 0;
+        }
+
+        if (!(d0 > (double) this.attackRadiusSqr) && this.seeTime >= 5) {
+            this.mob.getNavigation().stop();
+        } else {
+            this.mob.getNavigation().moveTo(this.target.getX(), target.getY() + 3, target.getZ(), 1);
+        }
+
+        this.mob.getLookControl().setLookAt(this.target, 30.0F, 30.0F);
+        if (--this.attackTime == 0) {
+            if (!flag) {
+                return;
+            }
+
+            float f = (float) Math.sqrt(d0) / this.attackRadius;
+            float f1 = Mth.clamp(f, 0.1F, 1.0F);
+            this.rangedAttackMob.performRangedAttack(this.target, f1);
+            this.attackTime = Mth.floor(f * (float) (this.attackIntervalMax - this.attackIntervalMin) + (float) this.attackIntervalMin);
+        } else if (this.attackTime < 0) {
+            this.attackTime = Mth.floor(Mth.lerp(Math.sqrt(d0) / (double) this.attackRadius, (double) this.attackIntervalMin, (double) this.attackIntervalMax));
         }
     }
 }
