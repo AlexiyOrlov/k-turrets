0790dde74d676061491988b64f46511c1e45caaa
AlexiyOrlov <alexiy.ov@gmail.com>
2024-11-18 21:11:36 +0200

    Centralize

--- src/main/java/dev/buildtool/kturrets/storage/StorageDroneScreen.java
+++ src/main/java/dev/buildtool/kturrets/storage/StorageDroneScreen.java
@@ -6,6 +6,7 @@ import dev.buildtool.kturrets.packets.*;
 import dev.buildtool.satako.gui.*;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
 
 import java.util.LinkedHashMap;
 
@@ -19,27 +20,31 @@ public class StorageDroneScreen extends Screen2 {
     @Override
     public void init() {
         super.init();
-        addRenderableWidget(new BetterButton(centerX, 20, Component.translatable("k_turrets.dismantle"), p_onPress_1_ -> {
+        MutableComponent dismantle = Component.translatable("k_turrets.dismantle");
+        addRenderableWidget(new BetterButton(centerX-font.width(dismantle.getString())/2, 20, dismantle, p_onPress_1_ -> {
             KTurrets.channel.sendToServer(new DismantleTurret(drone.getId()));
             minecraft.player.closeContainer();
         }));
-        addRenderableWidget(new SwitchButton(centerX, 40, Component.translatable("k_turrets.mobile"), Component.translatable("k_turrets.immobile"), drone.isMoveable(), p_onPress_1_ -> {
+        MutableComponent switchB = Component.translatable("k_turrets.immobile");
+        addRenderableWidget(new SwitchButton(centerX-font.width(switchB.getString())/2, 40, Component.translatable("k_turrets.mobile"), switchB, drone.isMoveable(), p_onPress_1_ -> {
             KTurrets.channel.sendToServer(new ToggleMobility(!drone.isMoveable(), drone.getId()));
             drone.setMoveable(!drone.isMoveable());
             if (p_onPress_1_ instanceof SwitchButton) {
                 ((SwitchButton) p_onPress_1_).state = !((SwitchButton) p_onPress_1_).state;
             }
         }));
-        if (!drone.getOwner().isPresent())
-            addRenderableWidget(new BetterButton(centerX, 60, Component.translatable("k_turrets.claim.drone"), p_onPress_1_ -> {
+        if (!drone.getOwner().isPresent()) {
+            MutableComponent claim = Component.translatable("k_turrets.claim.drone");
+            addRenderableWidget(new BetterButton(centerX-font.width(claim)/2, 60, claim, p_onPress_1_ -> {
                 KTurrets.channel.sendToServer(new ClaimTurret(drone.getId(), minecraft.player.getUUID()));
                 drone.setOwner(minecraft.player.getUUID());
                 minecraft.player.closeContainer();
             }));
-        else {
-            DropDownButton dropDownButton = new DropDownButton(centerX, 60, this, Component.literal(""));
+        } else {
+            MutableComponent followText = Component.translatable("k_turrets.following.owner");
+            DropDownButton dropDownButton = new DropDownButton(centerX-font.width(followText)/2, 60, this, Component.literal(""));
             LinkedHashMap<Component, Button.OnPress> linkedHashMap = new LinkedHashMap<>(3);
-            RadioButton follow = new RadioButton(centerX, 140, Component.translatable("k_turrets.following.owner"));
+            RadioButton follow = new RadioButton(centerX, 140, followText);
             linkedHashMap.put(follow.getMessage(), p_93751_ -> {
                 KTurrets.channel.sendToServer(new ToggleDroneFollow(true, drone.getId()));
                 drone.followOwner(true);
