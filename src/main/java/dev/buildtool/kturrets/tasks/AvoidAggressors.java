59b4f77082b74a7d95bcb02485144acc1c193cdc
AlexiyOrlov <alexiy.ov@gmail.com>
2024-12-11 10:56:21 +0200

    Add aggressor avoidance task

--- src/main/java/dev/buildtool/kturrets/tasks/AvoidAggressors.java
+++ src/main/java/dev/buildtool/kturrets/tasks/AvoidAggressors.java
@@ -3,6 +3,7 @@ package dev.buildtool.kturrets.tasks;
 import dev.buildtool.kturrets.Drone;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ai.goal.Goal;
+import net.minecraft.world.entity.player.Player;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,9 +17,13 @@ public class AvoidAggressors extends Goal {
 
     @Override
     public boolean canUse() {
-        List<Mob> aggressors = drone.level().getEntitiesOfClass(Mob.class, drone.getBoundingBox().inflate(5), mob -> mob.getTarget() == drone);
-        Optional<Mob> aggressor = aggressors.stream().filter(mob -> mob.distanceTo(drone) < 3).findAny();
-        return aggressor.isPresent();
+        Player owner=drone.level().getPlayerByUUID(drone.getOwnerUUID());
+        if(owner!=null) {
+            List<Mob> aggressors = drone.level().getEntitiesOfClass(Mob.class, drone.getBoundingBox().inflate(5), mob -> mob.getTarget() == owner);
+            Optional<Mob> aggressor = aggressors.stream().filter(mob -> mob.distanceTo(drone) < 3).findAny();
+            return aggressor.isPresent();
+        }
+        return false;
     }
 
     /**
