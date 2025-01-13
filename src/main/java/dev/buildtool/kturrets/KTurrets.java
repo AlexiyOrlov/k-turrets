5e83835186af3471a8b8f4b5d01278abacf0a63b
AlexiyOrlov <alexiy.ov@gmail.com>
2025-01-13 19:11:00 +0200

    Remove log line

--- src/main/java/dev/buildtool/kturrets/KTurrets.java
+++ src/main/java/dev/buildtool/kturrets/KTurrets.java
@@ -50,9 +50,11 @@ import net.minecraftforge.event.server.ServerStoppingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLEnvironment;
 import net.minecraftforge.network.NetworkEvent;
@@ -609,8 +611,7 @@ public class KTurrets {
                                 }
                             }
                         }
-                    } else
-                        logger.info("   Skipping regular loot pool");
+                    }
                 });
             }
         }
